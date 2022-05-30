c8b4bddb58d7c93b95ed210303243935a30059b8
Sayantan Khan <sayantangkhan@gmail.com>
2022-05-30 08:27:28 -0400

    Updated semantic search server

--- semantic-search-web-interface/semantic_search_web_interface/__init__.py
+++ semantic-search-web-interface/semantic_search_web_interface/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, redirect, request, url_for
 from wtforms import Form, SelectField, StringField, validators
 from .inference import semantic_query
+import logging
 
 GENRE_CHOICES = ['Any', 'Horror', 'SciFi']
 
 SEARCH_RESULTS = []
+
+logger = logging.getLogger('werkzeug')
+handler = logging.FileHandler('access.log')
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
 class SearchForm(Form):
@@ -25,16 +31,15 @@ def create_app():
         if request.method == 'POST':
             query = request.form['query']
             genre = request.form['genre']
-            print(query, genre)
+            logger.debug("Query: {0}".format(query))
+            logger.debug("Genre: {0}".format(genre))
             global SEARCH_RESULTS
             SEARCH_RESULTS = semantic_query(query, [])
-            print(SEARCH_RESULTS)
             return redirect('/result')
         return render_template('search.html', genre_choices=GENRE_CHOICES)
 
     @app.route('/result')
     def result_page():
-        print(SEARCH_RESULTS)
         return render_template('result.html', search_results=SEARCH_RESULTS)
 
     return app
